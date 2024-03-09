30b7a5b761fcafc22d9c8c8f56bcde5dcaaa698f
Rodrigo Lima <limarod.cunha@gmail.com>
2024-03-09 12:22:29 -0300

    cors origin url

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
   // origin: ["http://localhost:5173", "http://127.0.0.1:5173/", "https://stately-florentine-a13244.netlify.app"],
-  origin: ["*", "https://visionary-maamoul-16218e.netlify.app", "http://localhost:5173"],
+  origin: ["*", "https://foodapprocket.netlify.app", "http://localhost:5173"],
   credentials: true
 }));
 
